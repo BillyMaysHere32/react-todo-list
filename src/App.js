2624dbc77403492df08263f7f2c278a62e1a413c
BillyMaysHere32 <kgetz32@gmail.com>
2022-11-02 15:27:04 -0700

    added uuid library for unique keys for each todo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import TodoList from './TodoList';
+// uuid library to provide random ids
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todos, setTodos] = useState([])
-  
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos(prevTodos => {
+      // add new todos to prev and spread over array
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null;
+  }
+
   return (
     <div>
       <TodoList todos={todos}/>
-      <input type="text" />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick= {handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
     </div>
