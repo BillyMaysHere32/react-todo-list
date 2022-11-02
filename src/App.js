241c16d3deb7d40daeedac626af13239ab6a4c0d
BillyMaysHere32 <kgetz32@gmail.com>
2022-11-02 16:46:59 -0700

    created useEffect hooks for storing and retreiving todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 // uuid library to provide random ids
 import { v4 as uuidv4 } from 'uuid';
 
+const localStorageKey = 'todoApp.todos'
+
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  // call effect to load todos
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(localStorageKey));
+    if (storedTodos) setTodos( prevTodos => [...prevTodos, ...storedTodos] );
+  }, [])
+  // empty array makes effect run once when component mounts
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(todos))
+  }, [todos])
+  // anytime todos changes, useEffect is triggered
+
+  
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value;
